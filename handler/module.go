a5d0c083ba55d55938e9cacfd09461e9f361eabd
19971996 <salahuddinade@gmail.com>
2022-03-18 13:56:49 +0700

    versi 2

--- handler/module.go
+++ handler/module.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,4 +13,41 @@ func Nama(c *gin.Context) {
 	action := c.Param("action")
 	message := name + " is " + action
 	c.String(http.StatusOK, message)
+}
+
+// localhost:8080/product?judul=Daun-Jatuh&penulis=tere-liye&harga=10000&penerbit=gramedia&cetakan=5
+func ProductHandler(c *gin.Context) {
+	judul := c.Query("judul")
+	penulis := c.Query("penulis")
+	penerbit := c.Query("penerbit")
+	harga := c.Query("harga")
+	cetakan := c.Query("cetakan")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":   "ini buku",
+		"item name": judul,
+		"penulis":   penulis,
+		"penerbit":  penerbit,
+		"harga":     harga,
+		"cetakan":   cetakan,
+		"time":      time.Now(),
+	})
+}
+
+// localhost:8080/smartphone/smartphone/xiaomi/redmi-note-5/ram-4-gb/3000000
+func ProductSmartphone(c *gin.Context) {
+	merk := c.Param("merk")
+	jenis := c.Param("jenis")
+	spesifikasi := c.Param("spesifikasi")
+	harga := c.Param("harga")
+	namaproduk := c.Param("namaproduk")
+	c.JSON(http.StatusOK, gin.H{
+		"message":     "ini smartphone",
+		"item name":   namaproduk,
+		"merk":        merk,
+		"jenis":       jenis,
+		"spesifikasi": spesifikasi,
+		"harga":       harga,
+		"time":        time.Now(),
+	})
 }
--- main.go
+++ main.go
@@ -8,5 +8,9 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/user/:name/*action", handler.Nama)
+	// soal 2, 5 query string
+	r.GET("/product", handler.ProductHandler)
+	// soal 3, 5 query param
+	r.GET("/smartphone/:namaproduk/:merk/:jenis/:spesifikasi/:harga", handler.ProductSmartphone)
 	r.Run()
 }
